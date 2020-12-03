4c99f6774ca71f733a93f14caf93a50e16cd03f1
Qurban <qurbanali4747@gmail.com>
2020-12-03 16:43:45 +0500

    Create child component

--- contact_api/src/App.js
+++ contact_api/src/App.js
@@ -1,11 +1,14 @@
 // import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 import CounterContext from './components/CounterContext';
 import Parent from './components/Parent'
 
 function App() {
+
+  let countState = useState(0)
   return (
-    <CounterContext.Provider value={10}>
+    <CounterContext.Provider value={countState}>
     <div>
     
       <Parent/>
--- contact_api/src/components/Child.js
+++ contact_api/src/components/Child.js
@@ -3,12 +3,13 @@ import CounterContext from './CounterContext';
 
 const Child = (props) =>{
     let counterValue=useContext(CounterContext)
-    console.log(counterValue)
+    // console.log(counterValue)
     return(
         <div>
         <h1>this is child Class</h1>
-        <h1> hi {counterValue}  </h1>
+        <h1> Counter value is : {counterValue[0]}  </h1>
         <h1>{document.lastModified}</h1>
+        <button onClick={() => {counterValue[1](++counterValue[0])}}>Press Button</button>
    </div>
     )
 
